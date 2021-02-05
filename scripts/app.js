89e80677ca12a7e7efa4a85ee3e8387a02da457c
Roy Sizemore <rsizemore@swampcastle.home>
2021-02-05 15:10:51 -0500

    Added JS function to maintain copywrite year

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-
+// Add a dynamic glow to the most recently uploaded project
 
 // Function to automatically update the year. Used in <footer> for copywrite info
 const copywrite = () => document.write(new Date().getFullYear());
