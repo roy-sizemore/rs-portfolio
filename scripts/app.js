8e6643e26f234d4a70942233b17311ce7dd121b9
Roy Sizemore <rsizemore@swampcastle.home>
2021-02-06 23:55:22 -0500

    Added responsiveness, glowing cards, fixed broken link

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,12 @@
-// Add a dynamic glow to the most recently uploaded project
+// Places nav bar in hamburger menu when responsive class is active
+function myFunction() {
+    let x = document.getElementsByClassName("res-nav");
+    if (x.className === "res-nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "res-nav";
+    }
+  }
 
 // Function to automatically update the year. Used in <footer> for copywrite info
 const copywrite = () => document.write(new Date().getFullYear());
