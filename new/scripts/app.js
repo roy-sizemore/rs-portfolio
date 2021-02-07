b6e0e3b126066ebbaaad5da5b19477563d2dbae7
roy-sizemore <67349682+roy-sizemore@users.noreply.github.com>
2021-02-07 00:06:26 -0500

    Deleted extraneous folder

--- new/scripts/app.js
+++
@@ -1,5 +0,0 @@
-// Add a dynamic glow to the most recently uploaded project
-
-// Function to automatically update the year. Used in <footer> for copywrite info
-const copywrite = () => document.write(new Date().getFullYear());
-
