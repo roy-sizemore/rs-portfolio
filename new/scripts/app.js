13431df1b3b1643bf41bbe7d188483e319c09699
Roy Sizemore <rsizemore@swampcastle.home>
2021-02-06 23:59:18 -0500

    Added responsiveness, background image in header, fixed formatting and broken link

---
+++ new/scripts/app.js
@@ -0,0 +1,5 @@
+// Add a dynamic glow to the most recently uploaded project
+
+// Function to automatically update the year. Used in <footer> for copywrite info
+const copywrite = () => document.write(new Date().getFullYear());
+
