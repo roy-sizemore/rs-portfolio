cb8c1aa8430aac30df0ff82f8a56f4bfcaf07fdd
Roy Sizemore <rsizemore@swampcastle.home>
2021-05-10 13:57:33 -0400

    Added .gitignore; deleted legacy folder

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,12 @@
 // Places nav bar in hamburger menu when responsive class is active
-function myFunction() {
-    let x = document.getElementsByClassName("res-nav");
-    if (x.className === "res-nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "res-nav";
-    }
-  }
+// function myFunction() {
+//     let x = document.getElementsByClassName("res-nav");
+//     if (x.className === "res-nav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "res-nav";
+//     }
+//   }
 
 // Function to automatically update the year. Used in <footer> for copywrite info
 const copywrite = () => document.write(new Date().getFullYear());
